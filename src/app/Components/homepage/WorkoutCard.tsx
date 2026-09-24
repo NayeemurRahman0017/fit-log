2680d7ee9e67129ed10e7a9cadfd47ee4c9aceb9
Nayeembhuiyan0017 <nayeembhuiyan0017@gmail.com>
2026-09-25 00:36:21 +0600

    library added

--- src/app/Components/homepage/WorkoutCard.tsx
+++ src/app/Components/homepage/WorkoutCard.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+
+
 import Link from "next/link";
 import Image from "next/image";
 
@@ -9,7 +11,7 @@ import {
   Star,
 } from "../shared/icons";
 
-import type { Workout } from "../../types";
+import type { Workout } from "../../../types";
 
 export function WorkoutCard({
   workout,
